6e9bc2a669713a418fc8e97770a1abc37d082990
JannePiiroinen <amk1005446@student.hamk.fi>
2024-09-30 14:12:04 +0300

    changes to rightAnswer - variable

--- src/LoopAdvanced.java
+++ src/LoopAdvanced.java
@@ -6,7 +6,7 @@ public class LoopAdvanced {
         Scanner in = new Scanner(System.in);
 
         String input;
-        String rightAnswer = "Emma";
+        String rightAnswer = "Jokke";
         boolean correctGuess = false;
         int arvausLaskuri = 0;
 
