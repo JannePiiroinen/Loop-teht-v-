77dba6389f3cca98ac2c79919559a90d440b540f
Janne Piiroinen <janne.piiroinen1989@gmail.com>
2024-09-30 13:58:22 +0300

    tehtävä gittiin

---
+++ src/LoopAdvanced2.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class LoopAdvanced2 {
+    public static void main(String[] args) throws Exception {
+
+        Scanner in = new Scanner(System.in);
+
+        String input;
+        String rightAnswer = "Emma";
+        boolean correctGuess = false;
+        int arvausLaskuri = 0;
+
+        do {
+            System.out.println("Guess my name (type stop to exit)");
+            input = in.nextLine();
+
+            if (input.equalsIgnoreCase("stop")) {
+                System.out.println("You guessed " + arvausLaskuri + " times.");
+                break;
+            }
+                arvausLaskuri++;
+
+            if (input.equalsIgnoreCase(rightAnswer)) {
+                correctGuess = true;
+            }    
+
+                } while (!correctGuess); 
+
+             if (correctGuess) {   
+                System.out.println("Congratulations!");
+                System.out.println("You guessed " + arvausLaskuri + " times.");
+        }
+    }
+}
+
+        
+    
+
+
